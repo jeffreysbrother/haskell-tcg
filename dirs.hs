afa5779ac7d7783343ee1bae7d1509c1deeff002
James Cool <james.cool@thecontrolgroup.com>
2017-12-13 17:02:21 -0800

    added config, installed something, changed stuff

--- dirs.hs
+++ dirs.hs
@@ -2,8 +2,24 @@ import Data.List
 import System.IO
 import System.Directory
 import System.FilePath
+import Data.Aeson
+import qualified Data.ByteString.Lazy as B
+
+jsonFile :: FilePath
+jsonFile = "config.json"
+
+getJSON :: IO B.ByteString
+getJSON = B.readFile jsonFile
 
 cwd = getCurrentDirectory
-pathToSection = cwd ++ '/source/sections';
-pathToConfig = cwd ++ '/config.json';
--- devInitials = require(pathToConfig).developer;
+-- pathToSection = cwd ++ '/source/sections'
+-- pathToConfig = cwd ++ '/config.json'
+-- devInitials = require(pathToConfig).developer
+
+section :: FilePath
+section = "/source/sections"
+
+-- toText :: section -> Text
+-- flattenIO xs = fmap flatten xs
+-- encodeString :: FilePath -> String
+-- encodeString cwd = best
